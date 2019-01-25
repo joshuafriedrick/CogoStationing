b8b3aa8cddc405d528a892855553d4738ac30f6d
josh <josh@seedmaster.ca>
2019-01-24 23:38:56 -0600

    Add acad.exe path to debug and start CogoStation command and user input

---
+++ CogoStationing/UserInput.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace CogoStationing
+{
+    public class UserInput
+    {
+        public static ObjectId GetAlignmentId(ref Editor ed, string message)
+        {
+            PromptEntityOptions options = new PromptEntityOptions("\n" + message);
+            options.SetRejectMessage("\n You must select an alignment \n");
+            options.AddAllowedClass(typeof(Alignment), true);
+            ObjectId alignId = ed.GetEntity(options).ObjectId;
+            return alignId;
+        }
+    }
+}
